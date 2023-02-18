3fdb83d36cfddf5fb958d898a9385769569903ee
Nora Nwachukwu <chinomso@Noras-MacBook-Air.local>
2023-02-18 21:11:47 +0100

    first commit

---
+++ src/Components/Questions.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Questons = () => {
+    return (
+        <div>
+        <div>
+            <p>Is this a good product?</p>
+        < /div>
+    <div>
+        <p>How much does it cost?</p>
+    </div>
+    <div>
+        <p>Where can I get it?</p>
+    </div>
+        </div>
+)
+
+}
